bfce73110058cdad880436d0ba0bbe381ff7237f
Edwin <edwinjmenendez@gmail.com>
2020-09-23 22:31:44 -0700

    when published, description was added to json file; SafeAreaView  notes added

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
-  let x = 1;
+
   // console.log('App Excecuted') shows up in terminal
   return (
-    // view is like a div and any test must be rendered by a text component
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    // with SafeAreaView now <text> is no longer behind ipone notch, adding some padding 
+    <SafeAreaView style={styles.container}>
+      <Text>Hello Mom</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'dodgerblue',
+    // if these two are removed text is at the very top of iphone xr screen (bad)
+    // fixed by adding the SafeAreaView component
+    alignItems: 'center', 
     justifyContent: 'center',
   },
 });
