5ac8d3d8cfd5154b2caecdb9d0c865336bb38f9f
Edwin <edwinjmenendez@gmail.com>
2020-09-24 00:42:15 -0700

    orientation changes; app.json can change orientation to default to support both orientations; hooks dont work with me

--- App.js
+++ App.js
@@ -2,20 +2,29 @@ import React from 'react';
 import { 
   Dimensions,
   StyleSheet, 
-  Text, 
-  SafeAreaView, View,
-  Button, 
+  SafeAreaView, View, 
   Platform,
   StatusBar
 } from 'react-native';
 
+// these hooks dont work on my computer... figure out later
+// import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
+// useDimensions basicavlly gets the dimensions portrqait/horizontally
+//  useDeviceOrientation() is a boolean that returns an object i.e. portrait: true
+
 export default function App() {
-  console.log(Dimensions.get('screen')) // checks the dimensions of screen
+  // console.log(Dimensions.get('screen')) // checks the dimensions of screen
+  // console.log(useDeviceOrientation())
+
+  // const { landscape } = useDeviceOrientation()
+  // depending on orientation, it'll configure my height
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{
         width: '50%',
-        height: 70,
+        height: '70%',
+        // height: landscape ? '70%' : '30%', hook doesnt work on my device
         backgroundColor: 'dodgerblue'
       }} >
 
