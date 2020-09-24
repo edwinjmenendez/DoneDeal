cbed9379e261a293c733c407c6d2fbc5ae30bf00
Edwin <edwinjmenendez@gmail.com>
2020-09-24 00:58:45 -0700

    flexbox notes

--- App.js
+++ App.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import { 
-  Dimensions,
-  StyleSheet, 
-  SafeAreaView, View, 
-  Platform,
-  StatusBar
-} from 'react-native';
-
-// these hooks dont work on my computer... figure out later
-// import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
-// useDimensions basicavlly gets the dimensions portrqait/horizontally
-//  useDeviceOrientation() is a boolean that returns an object i.e. portrait: true
+import { View } from 'react-native';
 
 export default function App() {
-  // console.log(Dimensions.get('screen')) // checks the dimensions of screen
-  // console.log(useDeviceOrientation())
-
-  // const { landscape } = useDeviceOrientation()
-  // depending on orientation, it'll configure my height
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        width: '50%',
-        height: '70%',
-        // height: landscape ? '70%' : '30%', hook doesnt work on my device
-        backgroundColor: 'dodgerblue'
-      }} >
-
-      </View>
-    </SafeAreaView>
+    <View 
+      style={{
+        backgroundColor: 'dodgerblue',
+        flex: 1 // doesnt even show up!! btw this is the parent 
+      }}
+    >
+      <View 
+        style={{
+          backgroundColor: 'red',
+          flex: 1 // same size as gold
+        }}
+      ></View>
+      <View 
+        style={{
+          backgroundColor: 'black',
+          flex: 2 // double the size of the others
+        }}
+      ></View>
+      <View 
+        style={{
+          backgroundColor: 'gold',
+          flex: 1 // same size as red
+        }}
+      ></View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center', 
-    // justifyContent: 'center',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     // alignItems: 'center', 
+//     // justifyContent: 'center',
+//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+//   },
+// });
