08869e3fcd334d149bf51c5d99394271f0eebcaa
Edwin <edwinjmenendez@gmail.com>
2020-09-24 08:32:03 -0700

    justifyContent, alignItem/self

--- App.js
+++ App.js
@@ -5,16 +5,19 @@ export default function App() {
   return (
     <View 
       style={{
-        backgroundColor: 'dodgerblue',
+        backgroundColor: 'white',
         flex: 1, // doesnt even show up!! btw this is the parent
         flexDirection: 'row', // by defeault, the direction (without this) ios vertical not horizontal
+        justifyContent: 'center', // align item along the main axis; dependent on flexDir; space around/between etc // aligns main axis (in this case horizontally)
+        alignItems: 'center', // align item along the main axis; dependent on flexDir; space around/between etc // aligns secondary axis (in this case vertically)
       }}
     >
       <View 
         style={{
           backgroundColor: 'red',
           width: 100,
-          height: 200
+          height: 200,
+          alignSelf: 'flex-start', // aligns chosen element on 2ndary axis (i think) 
         }}
       ></View>
       <View 
