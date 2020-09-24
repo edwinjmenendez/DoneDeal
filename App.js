57b4c25a0185fe64f9f3fe17fff5e6112dfac29f
Edwin <edwinjmenendez@gmail.com>
2020-09-23 22:46:25 -0700

    Text component notes and functions; onPress

--- App.js
+++ App.js
@@ -5,10 +5,17 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 export default function App() {
 
   // console.log('App Excecuted') shows up in terminal
+  // handlePress will be on the actual text
+  const handlePress = () => console.log('same as onCLick i think')
+
   return (
     // with SafeAreaView now <text> is no longer behind ipone notch, adding some padding 
+    
+    // Initially what happens when you add a bunch of text is text 
+    // will go edge to edge and move onto next line
+    // but if you set a nuberOfLines prop it'll be ion one full line with ...
     <SafeAreaView style={styles.container}>
-      <Text>Hello Mom</Text>
+      <Text numberOfLines={1} onPress={handlePress}> Beyond Burger </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
