725c869cb66eb7aad12714f439ab8f9ad09de869
Edwin <edwinjmenendez@gmail.com>
2020-09-24 22:46:33 -0700

    each button is now positioned correctly

--- client/components/ViewImageScreen.js
+++ client/components/ViewImageScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, SafeAreaView, StyleSheet, Image } from 'react-native';
+import { View, SafeAreaView, StyleSheet, Image, Platform, StatusBar } from 'react-native';
 
 const ViewImageScreen = () => {
   return (
@@ -18,25 +18,27 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: '#000000',
-    flexDirection: 'row',
-    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain', // resizes image to fit on page
   },
   leftBox: {
     backgroundColor: '#fc5c65',
     width: 50,
     height: 50,
+    position: 'absolute',
+    top: Platform.OS === 'android' ? StatusBar.currentHeight + 40 : 40,
+    left: 30
   },
   rightBox: {
     backgroundColor: '#4ECDC4',
     width: 50,
-    height: 50
-  },
-  image: {
-    width: 400,
-    height: 700,
-    resizeMode: 'contain',
+    height: 50,
     position: 'absolute',
-    alignSelf: 'center',
+    top: Platform.OS === 'android' ? StatusBar.currentHeight + 40 : 40,
+    right: 30
   },
 })
 
