4048023b585c9d07479ffc855711371c81bc2be3
Edwin <edwinjmenendez@gmail.com>
2020-09-24 20:32:43 -0700

    started viewImageScreen

--- client/components/ViewImageScreen.js
+++ client/components/ViewImageScreen.js
@@ -1,2 +1,10 @@
 import { React } from "react";
+import { View, Text, SafeAreaView } from 'react-native';
 
+const ViewImageScreen = () => {
+  return (
+   <SafeAreaView>
+     <Text>Hello</Text>
+   </SafeAreaView> 
+  )
+}
