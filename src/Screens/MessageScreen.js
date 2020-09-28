cd53be826d5bc37befe5de996b5cbd60701529c2
Edwin <edwinjmenendez@gmail.com>
2020-09-28 15:09:40 -0700

    imported expo-constants to use for paddingTop; no longer need to import platform or statusBar api

--- src/Screens/MessageScreen.js
+++ src/Screens/MessageScreen.js
@@ -1,19 +1,47 @@
 import React from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native'
+import Constants from 'expo-constants';
+
+import ListItem from '../Components/ListItem'
+
+// dummyData
+
+const messages = [
+  {
+    id: 1,
+    title: 'T1',
+    description: 'HELLO',
+    image: require('../assets/mosh.jpg')
+  },
+  {
+    id: 2,
+    title: 'T2',
+    description: 'WORLD',
+    image: require('../assets/mosh.jpg')
+  },
+]
 
 const MessageScreen = () => {
+  // flatList takes 3 props
   return (
-    <View style={styles.container}>
-      <Text>Testing MessageScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container} >
+      <FlatList
+        data={messages}
+        keyExtractor={message => message.id.toString()} // converted to string because we'll get a warning
+        renderItem={({ item }) => 
+          <ListItem 
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />}
+      />
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
   }
 
 })
