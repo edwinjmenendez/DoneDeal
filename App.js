f82e38d6d3444cfc51c6ec8228b92b842110d911
Edwin <edwinjmenendez@gmail.com>
2020-09-29 15:54:48 -0700

    added a reusable icon component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform, SafeAreaView, ScrollView, View } from 'react-native';
+import { Platform, SafeAreaView, ScrollView, View, StyleSheet } from 'react-native';
 
 import WelcomeScreen from './src/Screens/WelcomeScreen';
 import ViewImageScreen from './src/Screens/ViewImageScreen.js';
@@ -8,11 +8,26 @@ import Card from './src/Components/Card'
 import AppText from './src/Components/AppText/AppText'
 import ListingDetailsScreen from './src/Screens/ListingDetailsScreen';
 import MessageScreen from './src/Screens/MessageScreen';
+import MyAccountScreen from './src/Screens/MyAccountScreen';
+import Screen from './src/Components/Screen';
+import colors from './src/config/colors';
+import Icon from './src/Components/Icon';
 
 export default function App() {
   return (
-      <MessageScreen />
+      <Screen style={styles.screen} >
+        <Icon 
+        name='email'
+        size={50}    
+        />
+      </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: colors.primary
+  }
+})
 
 // https://expo.io/@ejmenendez/DoneWithItPart2
