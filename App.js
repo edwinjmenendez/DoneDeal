0c57182bf2f222b6186584fbe1009798e5c0af20
Edwin <edwinjmenendez@gmail.com>
2020-10-12 23:53:59 -0700

    added a registerScreen that will contain name, email, password fields; note: i tried putting "user" for the icon and app would not run because it is not an icon from materialIconComm;

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import LoginScreen from './src/Screens/LoginScreen';
+import RegisterScreen from './src/Screens/RegisterScreen';
 
 
 
 export default function App() {
   return (
-    <LoginScreen />
+    <RegisterScreen />
   );
 }
 
---
+++ src/Screens/RegisterScreen.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import * as Yup from 'yup'
+import { StyleSheet } from 'react-native'
+
+import Screen from '../Components/Screen'
+import { AppForm, AppFormField } from '../Components/forms'
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label('Name'),
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).label('Password')
+})
+
+const RegisterScreen = () => {
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{name: '', email: '', password: ''}}
+        onSubmit={values => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          icon='account'
+          keyboardType='default'
+          name='Name'
+          placeholder='Name'
+          textContentType='name'
+        />
+      </AppForm>
+    </Screen>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // padding:  20, // padding dont work; not sure why
+    margin: 10,
+  },
+  logo: {
+     width: 80,
+     height: 80,
+     alignSelf: 'center',
+     marginTop: 50,
+     marginBottom: 20,
+  }
+})
+
+export default RegisterScreen
