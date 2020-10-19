0d6b7083b2bcaf5a09801d63507ba12f13432fe8
Edwin <edwinjmenendez@gmail.com>
2020-10-19 01:09:43 -0700

    rendering the ListingEditScreen on mobile phone

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import AppFormPicker from './src/Components/forms/AppFormPicker';
 import AppPicker from './src/Components/AppPicker'
+import { AppFormField } from './src/Components/forms';
 import Screen from './src/Components/Screen';
-
+import RegisterScreen from './src/Screens/RegisterScreen'
+import ListingEditScreen from './src/Screens/ListingEditScreen';
 
 export default function App() {
   return (
     <Screen>
-      <AppPicker />
+      <ListingEditScreen />
+      {/* <AppPicker /> */}
     </Screen>
   );
 }
