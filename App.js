27dbe2502223d94518237b9c327d049808cb9b41
Edwin <edwinjmenendez@gmail.com>
2020-11-18 00:52:17 -0800

    was trying to figure out why my screen style was not responding; removed <Screen> as a wrapped component in App.js

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-
-import AppFormPicker from './src/Components/forms/AppFormPicker';
-import AppPicker from './src/Components/AppPicker'
-import { AppFormField } from './src/Components/forms';
-import Screen from './src/Components/Screen';
-import RegisterScreen from './src/Screens/RegisterScreen'
-import ListingEditScreen from './src/Screens/ListingEditScreen';
-import ListItem from './src/Components/lists/ListItem';
-import MessageScreen from './src/Screens/MessageScreen';
-import Card from './src/Components/Card'
-import ListingScreen from './src/Screens/ListingScreen';
+import ViewImageScreen from './src/Screens/ViewImageScreen'
 
 export default function App() {
   return (
-    <Screen>
-      <ListingEditScreen />
-      {/* <AppPicker /> */}
-    </Screen>
+    <ViewImageScreen />
   );
 }
 
