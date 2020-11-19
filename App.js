905e67b30abbff40168238ec0fdb456d11163a1e
Edwin <edwinjmenendez@gmail.com>
2020-11-18 18:42:33 -0800

    requested  permissions to camera roll; testig ImagePicker from expo

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
 import ViewImageScreen from './src/Screens/ViewImageScreen'
+import Screen from './src/Components/Screen';
+import { Text, View } from 'react-native';
 
 export default function App() {
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    console.log(granted)
+    if (!granted) {
+      alert('You need to enable permission to access library')
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []) // passing in empty arr equivalent to CompDidMount; only updates once
+  
   return (
-    <ViewImageScreen />
+    <Screen>
+      <Text>Hello</Text>
+    </Screen>
   );
 }
 
