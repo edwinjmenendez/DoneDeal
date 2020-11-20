45d1a8a78e31e3d99e85e7769bdbe2b9655d7bf0
Edwin <edwinjmenendez@gmail.com>
2020-11-19 21:08:58 -0800

    added scrollable items for our list of pictures; useRef gave us access to instances of ScrollView Component

--- src/Components/lists/ImageInputList.js
+++ src/Components/lists/ImageInputList.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useRef } from 'react'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import ImageInput from '../ImageInput'
 
 const ImageInputList = ({ imageURIs = [], onAddImage, onRemoveImage }) => {
-  // whenever an image is selected we want to call the ImageInput again
+
+  // using ref hook, you can get the reference of the instance of a component
+  const scrollViewRef = useRef();
+  // scrollViewRef.current <- we now have access (see docs)
+  // to instances/methods of ScrollView since we defined it in our ref prop
 
   const uris = imageURIs.map(uri => // REMEMBER TO ALWAYS RETURN WITH MAP
     <View key={uri} style={styles.image}>
@@ -15,9 +19,18 @@ const ImageInputList = ({ imageURIs = [], onAddImage, onRemoveImage }) => {
   )
 
   return (
-    <View style={styles.container}>
-      {uris}
-      <ImageInput onChangeImage={uri => onAddImage(uri)} />
+    // view added because scrollView takes up whole space around it (try changing bckgnd color)
+    <View>
+      <ScrollView
+        horizontal
+        onContentSizeChange={() => scrollViewRef.current.scrollToEnd()}
+        ref={scrollViewRef}
+        >
+        <View style={styles.container}>
+          {uris}
+          <ImageInput onChangeImage={uri => onAddImage(uri)} />
+        </View>
+      </ScrollView>  
     </View>
   )
 }
