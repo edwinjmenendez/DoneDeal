ee838fcd280852d90e03dc4622103aeea86c2a9b
Edwin <edwinjmenendez@gmail.com>
2020-11-20 14:47:08 -0800

    testing out navigation with react nav;

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import ViewImageScreen from './src/Screens/ViewImageScreen'
 import Screen from './src/Components/Screen';
-import ImageInput from './src/Components/ImageInput';
-import ImageInputList from './src/Components/lists/ImageInputList';
-import FormImagePicker from './src/Components/forms/FormImagePicker';
-import ListingEditScreen from './src/Screens/ListingEditScreen';
+
 
 export default function App() {
+  // gives us access to the Screen/Nav keys which should be wrapped in a navContainer 
+  const Stack = createStackNavigator();
+
+  // dummy components
+  const Tweets = () => {
+    return (
+      <Screen>
+        <Text>Tweets</Text>
+      </Screen>
+    )
+  }
+  const TweetDetails = () => {
+    return (
+      <Screen>
+        <Text>Tweet Details</Text>
+      </Screen>
+    )
+  }
 
   return (
-    <ListingEditScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Tweets' >
+        <Stack.Screen name='TweetDetails' component={TweetDetails} />
+        <Stack.Screen name='Tweets' component={Tweets} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
 
 // https://expo.io/@ejmenendez/DoneWithItPart2
