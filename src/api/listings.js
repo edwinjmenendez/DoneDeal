843251530ce2a3ce955599cb149840aa6fba9f83
Edwin <edwinjmenendez@gmail.com>
2020-11-23 04:20:08 -0800

    created post request function to add listings on listings.js

--- src/api/listings.js
+++ src/api/listings.js
@@ -1,8 +1,33 @@
 import client from './client';
 
 const endpoint = '/listings'
+// get listings request
 const getListings = () => client.get(endpoint);
 
+const addListings = listing => {
+  // t’s encoded and sent out with Content-Type: multipart/form-data
+  const data = new FormData();
+  // append title, description, price, categoryid, images, location
+  data.append('title', listing.title);
+  data.append('price', listing.price);
+  data.append('categoryId', listing.categoryId);
+  data.append('description', listing.description);
+  
+  // since we can have more than one image
+  listing.images.forEach((image, index) => {
+    data.append('images', {
+      name: 'image' + index,
+      type: 'image/jpeg',
+      uri: image,
+    })
+  });
+
+  // location is optional
+  if (listing.location) data.append('location', JSON.stringify(listing.location));
+  // call post request
+  return client.post(endpoint, data);
+}
 export default {
+  addListings,
   getListings,
 }
